a56cc74ff1d0c930cf99963029965d889a230133
HarshilMendpara <mendparaharshil@gmail.com>
2021-12-23 20:20:38 +0530

    Created footer component

--- components/LandingPage/Footer/Footer.tsx
+++ components/LandingPage/Footer/Footer.tsx
@@ -1,9 +1,45 @@
-import type { NextPage } from "next";
-
 import styles from "./Footer.module.css";
 
-const Footer: NextPage = () => {
-  return <div>Footer</div>;
+import Image from "next/image";
+import facebookLogo from "../../../assets/landing/icons/facebook.svg";
+import instagramLogo from "../../../assets/landing/icons/instagram.svg";
+import twitterLogo from "../../../assets/landing/icons/twitter.svg";
+import linkedinLogo from "../../../assets/landing/icons/linkedin.svg";
+
+const Footer = () => {
+  return (
+    <div className={styles.FooterContainer}>
+      <p className={styles.ComingText}>coming</p>
+      <p className={styles.SoonText}>soon</p>
+      <p className={styles.SocialMediaText}>
+        meanwhile you can check out
+        <br />
+        our social media handles :
+      </p>
+      <div className={styles.SocialMediaContainer}>
+        <Image
+          className={styles.SocialMediaIcon}
+          src={facebookLogo}
+          alt="facebook"
+        />
+        <Image
+          src={instagramLogo}
+          alt="instagram"
+          className={styles.SocialMediaIcon}
+        />
+        <Image
+          src={linkedinLogo}
+          alt="linkedin"
+          className={styles.SocialMediaIcon}
+        />
+        <Image
+          src={twitterLogo}
+          alt="twitter"
+          className={styles.SocialMediaIcon}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
