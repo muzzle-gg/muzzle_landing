4ed526e2e1ab2dfb54de514af0327961f01347a3
Raj <varasaniraj5@gmail.com>
2021-12-24 14:52:25 +0530

    Added helper import all

---
+++ utils/helpers/ImportAll.ts
@@ -0,0 +1,7 @@
+export const importAll = (r: any) => {
+  let images: any[] = [];
+  r.keys().map((item: any, index: number) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+};
