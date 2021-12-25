00fa76d739cc4861ae654a0a523f2b6a064aa7ea
Raj <varasaniraj5@gmail.com>
2021-12-25 12:31:38 +0530

    Created content component for lower sec
    Co-authored-by: Harshil Mendpara <HarshilMendpara@users.noreply.github.com>
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

---
+++ components/LandingPage/LowerSec/Content/Content.tsx
@@ -0,0 +1,34 @@
+import styles from "./Content.module.css";
+
+import { lowerSecInfo as contentData } from "../../../../utils/constants/StaticData";
+
+type ContentProps = {
+  currentActiveIndex: number;
+};
+
+export default function Content({ currentActiveIndex }: ContentProps) {
+  return (
+    <div className={styles.Wrapper}>
+      {contentData.map((data, index) => {
+        return (
+          <div
+            className={styles.Content}
+            key={index}
+            style={
+              currentActiveIndex === index
+                ? {
+                    opacity: 1,
+                  }
+                : {
+                    opacity: 0,
+                  }
+            }
+          >
+            <h3 className={styles.ContentTitle}>{data.title}</h3>
+            <div className={styles.ContentDesc}>{data.description}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
