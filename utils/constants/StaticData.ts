002e3d6476914677b85372d991e89c83ec3dd2c0
Raj <varasaniraj5@gmail.com>
2021-12-25 12:30:07 +0530

    Updated static data
    Co-authored-by: Harshil Mendpara <HarshilMendpara@users.noreply.github.com>
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

---
+++ utils/constants/StaticData.ts
@@ -0,0 +1,23 @@
+import logo from "../../assets/_General/Logo.svg";
+
+export const images = {
+  logo: logo,
+};
+
+export const lowerSecInfo = [
+  {
+    title: "Gaming News and Updates",
+    description:
+      "Get your info tests delivered at home collect a sample from the your progress tests.",
+  },
+  {
+    title: "Personal Profile",
+    description:
+      "Get your info tests delivered at home collect a sample from the your progress tests.",
+  },
+  {
+    title: "Upcoming Tournaments",
+    description:
+      "Get your info tests delivered at home collect a sample from the your progress tests.",
+  },
+];
