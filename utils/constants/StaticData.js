8a628516a36ddbbb92c9a10ab1e916b52c8d306f
Akash Gupta <akash.gupta.anup@gmail.com>
2021-12-25 13:12:50 +0530

    Added static data and hue animation on footer
    Co-authored-by: Harshil Mendpara <HarshilMendpara@users.noreply.github.com>
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>

--- utils/constants/StaticData.js
+++ utils/constants/StaticData.js
@@ -1,5 +1,19 @@
 import logo from "../../assets/_General/Logo.svg";
 
+export const content = {
+  landingPage: {
+    upperContainer: {
+      title: [
+        "Your",
+        "first step towards",
+        "removing the recoil from",
+        "the gaming world",
+      ],
+      subTitle: "Coming Soon",
+    },
+  },
+};
+
 export const images = {
   logo: logo,
 };
